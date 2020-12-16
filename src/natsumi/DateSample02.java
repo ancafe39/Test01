ba65457c6a7a0d554df860c48cf92161cd5e2c9c
Honma Natsumi <ancafe39n@gmail.com>
2020-12-16 15:13:04 +0900

    ある日付のN日前

---
+++ src/natsumi/DateSample02.java
@@ -0,0 +1,16 @@
+package natsumi;
+
+import java.util.Calendar;
+public class DateSample02 {
+  public static void main(String[] args) {
+
+    Calendar cal = Calendar.getInstance();
+    cal.set(2009, Calendar.APRIL, 25, 6, 44, 0);
+    int day = 100;
+    System.out.print( String.format(
+      "%1$tY年 %1$tB %1$te日 %1$ta曜日の%2$d日前は、",cal,day));
+    cal.add(Calendar.DAY_OF_YEAR, -day);
+    System.out.println( String.format(
+      "%1$tY年 %1$tB %1$te日 %1$ta曜日です。", cal));
+  }
+}
